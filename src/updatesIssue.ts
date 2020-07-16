e095e83e8514b7666ad54e56ac7956fe32e5321a
Orta <git@orta.io>
2020-07-16 16:50:36 -0400

    UI polish

--- src/updatesIssue.ts
+++ src/updatesIssue.ts
@@ -13,7 +13,7 @@ export type EmbeddedTwoslashRun = {
 }
 
 export const updateIssue = async (_ctx: Context, issue: Issue, newRuns: TwoslashResults[], api: API) => {
-  console.log('\nUpdating issue: ', issue.number, '')
+  process.stdout.write(`\nUpdating issue ${issue.number}: ``)
   if (newRuns.length === 0) return
 
   await updateMainComment(newRuns, api, issue)

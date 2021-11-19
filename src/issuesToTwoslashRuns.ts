807e22a2e9ac9fda189efbad966e766bd67b9082
Orta <git@orta.io>
2021-11-19 14:40:16 +0000

    Update

--- src/issuesToTwoslashRuns.ts
+++ src/issuesToTwoslashRuns.ts
@@ -13,37 +13,39 @@ export type TwoslashRun = {
   }[]
 }
 
-export const issueToTwoslashRun = (ctx: Context) => (issue: Issue): TwoslashRun => {
-  // Body -> CodeBlocks
-  const bodyCode = markdownToCodeBlocks(issue.body)
-  const codeBlocks: TwoslashRun['codeBlocksToRun'] = bodyCode.filter(validCodeblocks(ctx.tag)).map((c, i) => ({
-    block: c,
-    description: `<a href='#issue-${issue.databaseId}'>Issue body</a> code block by @${issue.author.login}`
-  }))
+export const issueToTwoslashRun =
+  (ctx: Context) =>
+  (issue: Issue): TwoslashRun => {
+    // Body -> CodeBlocks
+    const bodyCode = markdownToCodeBlocks(issue.body)
+    const codeBlocks: TwoslashRun['codeBlocksToRun'] = bodyCode.filter(validCodeblocks(ctx.tag)).map((c, i) => ({
+      block: c,
+      description: `<a href='#issue-${issue.databaseId}'>Issue body</a> code block by @${issue.author.login}`
+    }))
 
-  // Comment -> CodeBlocks[]
-  const commentCodeBlocks = issue.comments.nodes.map(c => ({
-    twoslashRuns: markdownToCodeBlocks(c.body).filter(validCodeblocks(ctx.tag)),
-    commentID: c.id,
-    description: `<a href='${c.url}'>Comment</a> by @${c.author.login}</a>`
-  }))
+    // Comment -> CodeBlocks[]
+    const commentCodeBlocks = issue.comments.nodes.map(c => ({
+      twoslashRuns: markdownToCodeBlocks(c.body).filter(validCodeblocks(ctx.tag)),
+      commentID: c.id,
+      description: `<a href='${c.url}'>Comment</a> by @${c.author.login}</a>`
+    }))
 
-  // Flatten to Comment -> CodeBlock
-  //            Comment -> CodeBlock
-  commentCodeBlocks.forEach(comment => {
-    comment.twoslashRuns.forEach(run => {
-      codeBlocks.push({
-        commentID: comment.commentID,
-        block: run,
-        description: comment.description
+    // Flatten to Comment -> CodeBlock
+    //            Comment -> CodeBlock
+    commentCodeBlocks.forEach(comment => {
+      comment.twoslashRuns.forEach(run => {
+        codeBlocks.push({
+          commentID: comment.commentID,
+          block: run,
+          description: comment.description
+        })
       })
     })
-  })
 
-  return {
-    issueNumber: issue.number,
-    codeBlocksToRun: codeBlocks
+    return {
+      issueNumber: issue.number,
+      codeBlocksToRun: codeBlocks
+    }
   }
-}
 
 const validCodeblocks = (tag: string) => (codeBlock: CodeBlock) => codeBlock.tags.includes(tag)
--- src/runTwoslashRuns.ts
+++ src/runTwoslashRuns.ts
@@ -51,55 +51,54 @@ export const runTwoSlashOnOlderVersions = (run: TwoslashRun['codeBlocksToRun'][n
 
 // TODO: Timeouts?
 //
-export const runTwoSlash = (label: string) => (
-  run: TwoslashRun['codeBlocksToRun'][number],
-  ts?: any
-): TwoslashResults => {
-  let result: ReturnType<typeof twoslasher>
-  const start = new Date()
-  const getTime = () => Math.round(new Date().getTime() - start.getTime())
+export const runTwoSlash =
+  (label: string) =>
+  (run: TwoslashRun['codeBlocksToRun'][number], ts?: any): TwoslashResults => {
+    let result: ReturnType<typeof twoslasher>
+    const start = new Date()
+    const getTime = () => Math.round(new Date().getTime() - start.getTime())
 
-  // TypeScript dep needs to be looked up by the workflow define parts of the FS first
-  const typeScripts = ['/home/runner/work/TypeScript/TypeScript/node_modules/typescript']
-  const t = typeScripts.find(tpath => existsSync(tpath)) || 'typescript'
+    // TypeScript dep needs to be looked up by the workflow define parts of the FS first
+    const typeScripts = ['/home/runner/work/TypeScript/TypeScript/node_modules/typescript']
+    const t = typeScripts.find(tpath => existsSync(tpath)) || 'typescript'
 
-  const tsModule = typeof ts === 'object' ? ts : require(t)
+    const tsModule = typeof ts === 'object' ? ts : require(t)
 
-  try {
-    const opts = {noErrorValidation: true, noStaticSemanticInfo: true}
-    result = twoslasher(run.block.content, run.block.lang, {defaultOptions: opts, tsModule})
-  } catch (error: any) {
-    return {
-      assertions: [],
-      fails: [],
-      exception: error.name + ' - ' + error.message + '\n\n```\n' + error.stack + '\n```\n\n',
+    try {
+      const opts = {noErrorValidation: true, noStaticSemanticInfo: true}
+      result = twoslasher(run.block.content, run.block.lang, {defaultOptions: opts, tsModule})
+    } catch (error: any) {
+      return {
+        assertions: [],
+        fails: [],
+        exception: error.name + ' - ' + error.message + '\n\n```\n' + error.stack + '\n```\n\n',
+        time: getTime(),
+        label,
+        commentID: run.commentID,
+        state: RunState.RaisedException,
+        description: run.description
+      }
+    }
+
+    const fails = result.errors.map(e => e.renderedMessage)
+
+    let state = RunState.Green
+    if (result.queries.length) state = RunState.HasAssertions
+    if (fails.length) state = RunState.CompileFailed
+
+    const returnResults: TwoslashResults = {
+      fails,
+      assertions: result.queries.map(q => q.text || q.completions!.map(q => q.name).join(', ')),
+      emit: result.code,
       time: getTime(),
       label,
       commentID: run.commentID,
-      state: RunState.RaisedException,
+      state,
       description: run.description
     }
+
+    const showEmit = run.block.content.includes('// @showEmit') // this would also hit @showEmittedFiles only
+    if (!showEmit) delete returnResults['emit']
+
+    return returnResults
   }
-
-  const fails = result.errors.map(e => e.renderedMessage)
-
-  let state = RunState.Green
-  if (result.queries.length) state = RunState.HasAssertions
-  if (fails.length) state = RunState.CompileFailed
-
-  const returnResults: TwoslashResults = {
-    fails,
-    assertions: result.queries.map(q => q.text || q.completions!.map(q => q.name).join(', ')),
-    emit: result.code,
-    time: getTime(),
-    label,
-    commentID: run.commentID,
-    state,
-    description: run.description
-  }
-
-  const showEmit = run.block.content.includes('// @showEmit') // this would also hit @showEmittedFiles only
-  if (!showEmit) delete returnResults['emit']
-
-  return returnResults
-}

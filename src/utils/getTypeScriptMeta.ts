d22267c0d992b9f2db964254ea88eb1c60c22d6a
Andrew Branch <andrew@wheream.io>
2022-04-11 11:36:06 -0700

    Let Node resolve TypeScript

--- src/utils/getTypeScriptMeta.ts
+++ src/utils/getTypeScriptMeta.ts
@@ -2,12 +2,9 @@ import { join } from "path"
 import { readFileSync } from "fs"
 import fetch from "node-fetch"
 
-export const  getTypeScriptMeta = async () => {
-  const pkgPath = join(__dirname, "..", "..", "node_modules", "typescript", "package.json")
-  const pkgJSON = JSON.parse(readFileSync(pkgPath, "utf8"))
-
+export const getTypeScriptMeta = async () => {
+  const pkgJSON = require("typescript/package.json");
   const version = pkgJSON.version
-
   const npmInfo = await fetch(`https://registry.npmjs.org/typescript/${version}`);
   const res = await npmInfo.json()
 

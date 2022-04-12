621e242f9b811b07d993a7584ae71bb302991284
Andrew Branch <andrew@wheream.io>
2022-04-12 12:08:22 -0700

    Fix test

--- src/__tests__/runTwoSlash.test.ts
+++ src/__tests__/runTwoSlash.test.ts
@@ -6,21 +6,21 @@ const maybeTest = isWatch ? test.skip : test
 
 maybeTest('handles a crashing test', () => {
   const broken = makeRun(`const a = 123; a = 321`)
-  const result = runTwoSlash('test')(broken)
+  const result = runTwoSlash('test')(broken, require("typescript"))
 
   expect(result.fails[0]).toContain(`Cannot assign to `)
 })
 
 maybeTest('handles a showEmit test', () => {
   const showJS = makeRun(`// @showEmit\nconst a: number = 123`)
-  const result = runTwoSlash('test')(showJS)
+  const result = runTwoSlash('test')(showJS, require("typescript"))
 
   expect(result.emit).toEqual(`"use strict";\nconst a = 123;\n`)
 })
 
 maybeTest('handles a query test', () => {
   const showJS = makeRun(`const abcde: number = 123\n//      ^?`)
-  const result = runTwoSlash('test')(showJS)
+  const result = runTwoSlash('test')(showJS, require("typescript"))
 
   expect(result.assertions).toEqual(['const abcde: number'])
 })
